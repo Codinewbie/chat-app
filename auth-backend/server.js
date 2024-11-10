888db58e90163642528ebea3f64613e138d2a837
Amankumar <amanks2580@gmail.com>
2024-11-10 22:53:05 +0530

    deploying

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -12,13 +12,7 @@ const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin:'*',
     credentials: true,
   })
 );
